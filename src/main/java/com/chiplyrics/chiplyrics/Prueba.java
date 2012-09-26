9a8a1c69dfb0c0a9d29ef60d8332601f5ef95632
Acefkor <cedale20038@hotmail.com>
2012-09-25 23:06:47 -0500

    Se crea el repositorio

---
+++ src/main/java/com/chiplyrics/chiplyrics/Prueba.java
@@ -0,0 +1,21 @@
+package com.chiplyrics.chiplyrics;
+
+/**
+ *
+ * @author acefkor
+ */
+public class Prueba {
+    
+    private String mensaje;
+
+    public Prueba() {
+    }
+    
+    public void accionBoton(){
+        mensaje = "Este es un mensaje de prueba, baka";
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+}
